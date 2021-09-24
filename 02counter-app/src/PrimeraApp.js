31eb8a1506d5a8a6b29eb59b83754539074f9a59
inghgalvanIA <inghgalvan@gmail.com>
2021-09-23 19:48:41 -0500

    inicio mi primera app

---
+++ 02counter-app/src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+
+const PrimeraApp = ()=>{
+
+    return ( 
+        <>
+            <h1>Hola mundo desde react</h1>
+            <p> mi primera app con react</p>
+        </>
+    );
+}
+
+export default PrimeraApp
